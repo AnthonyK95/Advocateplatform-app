728c45c166300822693673f816d8245b73a6211d
AnthonyK95 <antonioskritsas@outlook.com>
2018-11-28 08:08:23 +0200

    working progress

--- ui_development/src/components/devprops.vue
+++ ui_development/src/components/devprops.vue
@@ -2,7 +2,11 @@
     <div class="container">
         <h1>Device Properties</h1>
         <div class="main-elements">
-            
+            <ul>
+                <li v-for="props in deviceprops" :key="props._id">
+                    {{props.deviceName}}
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -29,7 +33,7 @@ export default {
             let deviceID = this.properties;
             axios.post('http://46.103.120.51:1540/api/device/properties',{deviceID},localStorage.getItem('token'))
             .then(response => {
-                this.deviceprops = response.data.properties;
+                this.deviceprops = response.data;
                 console.log(response);
            })
             .catch(error => {
